82f8369b8b859f76e2ffe120acc63b072094b90c
Husaam Khan <husaamkhan04@gmail.com>
2026-01-19 13:48:21 -0500

    Added input parameter check for ROM file path.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,17 @@ use std::{
 mod gameboy;
 mod cpu;
 use gameboy::Gameboy;
+
+const NUM_ARGS: usize = 2;
     
 fn main() {
     let args: Vec<String> = env::args().collect();
+    
+    if args.len() < NUM_ARGS {
+        eprintln!("Error: No ROM provided. Please provide the path to a Gameboy ROM.");
+        process::exit(1);
+    }
+    
     let rom_path = &args[1];
     
     let gb = Gameboy::new();
