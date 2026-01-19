a0cf78d6b0eaf315e878df8f82cc9bde0012c6ee
Husaam Khan <husaamkhan04@gmail.com>
2026-01-19 15:39:46 -0500

    Load ROM data into cartridge
    Added a cartridge struct to gameboy.rs which will hold data.
    Added logic to Gameboy::load_rom() to read in data from ROM file
    to cartridge rom field.

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -1,4 +1,5 @@
 use std::{
+    io::Read,
     fs::File,
     io::Error
 };
@@ -9,17 +10,38 @@ use crate::cpu::CPU;
 //  - Define libraries for the individual HW components
 //  - Complete GB struct definition
 
+struct Cartridge {
+    rom: Vec<u8>   
+}
+
+impl Cartridge {
+    fn new() -> Cartridge {
+        Cartridge { rom: Vec::new() }
+    }
+
+    fn load_rom(&mut self, data: Vec<u8>) {
+        self.rom = data;
+    }
+}
+
 pub struct Gameboy {
-    cpu: CPU    
+    cartridge: Cartridge,
+    cpu: CPU
 }
 
 impl Gameboy {
     pub fn new() -> Gameboy {
-        Gameboy { cpu: CPU::new() }
+        Gameboy { cartridge: Cartridge::new(),  cpu: CPU::new() }
     }
 
-    pub fn load_rom(&self, filepath: &str) -> Result<(), Error> {
-        let file = File::open(filepath)?;
+    pub fn load_rom(&mut self, filepath: &str) -> Result<(), Error> {
+        let mut file = File::open(filepath)?;
+
+        let mut data: Vec<u8> = Vec::new();
+        file.read_to_end(&mut data)?;
+
+        self.cartridge.load_rom(data);
+        
         Ok(())
     }
 }
