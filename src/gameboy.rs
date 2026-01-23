a8f0cfc8f598eedb277925049e3f41c4b673f255
Husaam Khan <husaamkhan04@gmail.com>
2026-01-22 19:52:44 -0500

    Removed Cartridge struct from gameboy.rs

--- src/gameboy.rs
+++ src/gameboy.rs
@@ -10,28 +10,14 @@ use crate::cpu::CPU;
 //  - Define libraries for the individual HW components
 //  - Complete GB struct definition
 
-struct Cartridge {
-    rom: Vec<u8>   
-}
-
-impl Cartridge {
-    fn new() -> Cartridge {
-        Cartridge { rom: Vec::new() }
-    }
-
-    fn load_rom(&mut self, data: Vec<u8>) {
-        self.rom = data;
-    }
-}
-
 pub struct Gameboy {
-    cartridge: Cartridge,
+    rom: Vec<u8>,
     cpu: CPU
 }
 
 impl Gameboy {
     pub fn new() -> Gameboy {
-        Gameboy { cartridge: Cartridge::new(),  cpu: CPU::new() }
+        Gameboy { rom: Vec::new(), cpu: CPU::new() }
     }
 
     pub fn load_rom(&mut self, filepath: &str) -> Result<(), Error> {
@@ -40,7 +26,7 @@ impl Gameboy {
         let mut data: Vec<u8> = Vec::new();
         file.read_to_end(&mut data)?;
 
-        self.cartridge.load_rom(data);
+        self.rom = data;
         
         Ok(())
     }
