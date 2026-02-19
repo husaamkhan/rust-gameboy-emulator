3ab8b2f376b6824c7950998526cf2778d5db6994
Husaam Khan <husaamkhan04@gmail.com>
2026-02-18 19:09:09 -0500

    Added CPU register and flag structs

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,26 @@
-pub struct CPU;
+struct Flags {
+    zero: bool,
+    subtraction: bool,
+    half_carry: bool,
+    carry: bool
+}
+
+struct Registers {
+    a: u8,
+    flags: Flags,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    h: u8,
+    l: u8,
+    sp: u16,
+    pc: u16
+}
+
+pub struct CPU {
+    registers: Registers
+}
 
 impl CPU {
     pub fn new() -> CPU {
