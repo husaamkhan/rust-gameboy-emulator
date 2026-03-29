41fa8f8414826d84b5412eb6afb00ac938628da4
Husaam Khan <husaamkhan04@gmail.com>
2026-03-29 13:45:19 -0400

    Added external ram, hram, and work ram
    Added ram fields as Vec<u8> to Memory

--- src/memory.rs
+++ src/memory.rs
@@ -1,22 +1,41 @@
+const HRAM_START: u16 =         0xFF80; // 127 bytes of High RAM
+const HRAM_END: u16 =           0xFFFE;
+const EXTERN_RAM_START: u16 =   0xA000; // 8 KiB of external RAM in a Gameboy Colour
+const EXTERN_RAM_END: u16 =     0xBFFF; // cartridge
+const WRAM_START: u16 =         0xC000; // 32 KiB of Work RAM, provided through a fixed
+const WRAM_END: u16 =           0xDFFF; // 4 KiB unit, and 7 switchable banks
+
+
 pub struct Memory {
-    rom: Vec<u8>
+    rom: Vec<u8>,
+    hram: Vec<u8>,
+    external_ram: Vec<u8>,
+    ram: Vec<u8>
 }
 
 impl Memory {
     pub fn new() -> Memory {
-        Memory { rom: Vec::new() }
+        Memory {
+            rom: Vec::new(),
+            hram: Vec::new(),
+            external_ram: Vec::new(),
+            ram: Vec::new()
+        }
     }
 
     pub fn load_rom(&mut self, data: Vec<u8>) {
         self.rom = data;
     }
 
+    // TODO: Is this function really needed?
     pub fn get_rom(&self) -> Vec<u8> {
         self.rom.clone()
     }
 
     pub fn fetch_byte(&self, index: u16) -> u8 {
-        *self.rom.get(index as usize).expect("Invalid memory access! PC is out of bounds.")
+        *self.rom.
+            get(index as usize).
+            expect("Invalid memory access! PC is out of bounds.")
     }
 }
 
